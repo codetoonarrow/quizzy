75497af9ea4cce1df9f35bf6c70626a04f51fdcf
Luke Plourde <luke@krown.com>
2020-08-28 14:58:37 -0400

    Basic Svelte quiz

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,13 @@
 <script>
-	export let name;
+	import Quiz from "./Quiz.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Quiz />
+<h4>Im a H4</h4>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	:global(h4){
+		color: green;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/Quiz.svelte
@@ -0,0 +1,27 @@
+<script>
+    let result = "";
+    let correctAnswer = "b";
+    let answers = ["a", "b", "c", "d"]
+
+    function pickAnswer(answer) {
+        if (answer === correctAnswer){
+            return result = "Correct!"
+        }
+        result = "Wrong!";
+    }
+</script>
+
+<div>
+    {#if result}
+        <h4>{result}</h4>
+    {:else}
+        <h5>Please pick an answer</h5>
+    {/if}
+    {#each answers as answer}
+        <button on:click={() => pickAnswer(answer)}>Answer {answer.toUpperCase()}</button>
+    {/each}
+</div>
+
+<style>
+
+</style>
